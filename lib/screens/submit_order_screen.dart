fa879972f694c4971a96c275e609176e8b0ab877
Mimo <mimoriam420@gmail.com>
2025-04-22 13:15:15 +0500

    feat(submit_order): bugfix

--- lib/screens/submit_order_screen.dart
+++ lib/screens/submit_order_screen.dart
@@ -29,13 +29,53 @@ class _SubmitOrderScreenState extends State<SubmitOrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF128f8b),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF128f8b),
-        title: const Text("Roadee"),
-        actions: [Text("You are logged in as "), CircleAvatar()],
+      // backgroundColor: const Color(0xFF128f8b),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.menu, color: Colors.black),
+            onPressed: () {},
+          ),
+          title: Row(
+            children: [
+              const Text(
+                'Roadie',
+                style: TextStyle(
+                  fontFamily: 'Cursive',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                  fontSize: 24,
+                ),
+              ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'You Are Logged In',
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                  const Text(
+                    'As Tamela S.',
+                    style: TextStyle(fontSize: 12, color: Colors.black),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 8),
+              const CircleAvatar(
+                radius: 18,
+                backgroundImage: AssetImage(
+                  'assets/user_placeholder.png',
+                ), // Add placeholder
+              ),
+            ],
+          ),
+        ),
       ),
-      drawer: Drawer(),
       body: SafeArea(
         child: Stack(
           children: [
