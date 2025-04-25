8061ef4db80356108572ba185d4f96f7c3388f59
Mimo <mimoriam420@gmail.com>
2025-04-26 02:55:39 +0500

    feat(home): confirmation on app exit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:roadee_flutter/draft_local/draft_login.dart';
+import 'package:roadee_flutter/screens/enter_info_screen.dart';
 import 'package:roadee_flutter/screens/payment_checkout_screen.dart';
 import 'package:roadee_flutter/screens/submit_order_screen.dart';
 import 'package:roadee_flutter/screens/user_profile_screen.dart';
@@ -56,187 +57,225 @@ class _HomeScreenState extends State<HomeScreen> {
           return Center(child: CircularProgressIndicator());
         }
         final user = snapshot.data!;
-        return Scaffold(
-          // backgroundColor: const Color(0xFF128f8b),
-          backgroundColor: Colors.white,
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(60),
-            child: AppBar(
-              backgroundColor: Colors.white,
-              elevation: 0,
-              leading: IconButton(
-                icon: const Icon(Icons.menu, color: Colors.black),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SubmitOrderScreen(),
-                    ),
+        return PopScope(
+          canPop: false,
+          onPopInvokedWithResult: (bool didPop, Object? result) async {
+            if (didPop) {
+              // If the route is popped, exit the app
+            } else {
+              // Show a confirmation dialog before allowing the pop
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Confirm Exit"),
+                    content: Text("Are you sure you want to exit the app?"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Don't exit
+                        },
+                        child: Text("Cancel"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Close dialog
+                          Navigator.of(context).pop(); // Pop the route
+                        },
+                        child: Text("Exit"),
+                      ),
+                    ],
                   );
                 },
-              ),
-              title: Row(
-                children: [
-                  const Text(
-                    'Roadie',
-                    style: TextStyle(
-                      fontFamily: 'Cursive',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                      fontSize: 24,
-                    ),
-                  ),
-                  const Spacer(),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'You Are Logged In',
-                        style: TextStyle(fontSize: 12, color: Colors.black54),
+              );
+            }
+          },
+          child: Scaffold(
+            // backgroundColor: const Color(0xFF128f8b),
+            backgroundColor: Colors.white,
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(60),
+              child: AppBar(
+                backgroundColor: Colors.white,
+                elevation: 0,
+                leading: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.black),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SubmitOrderScreen(),
                       ),
-                      Text(
-                        "${user['username']}",
-                        style: TextStyle(fontSize: 12, color: Colors.black),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 8),
-                  MenuAnchor(
-                    controller: _menuController,
-                    style: MenuStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.white),
-                    ),
-                    menuChildren: [
-                      MenuItemButton(
-                        onPressed: () => _onSelected('Your Profile'),
-                        child: Text('Your Profile'),
-                      ),
-                      MenuItemButton(
-                        onPressed: () => _onSelected('Settings'),
-                        child: Text('Settings'),
-                      ),
-                      MenuItemButton(
-                        onPressed: () => _onSelected('Log out'),
-                        child: Text('Log out'),
-                      ),
-                    ],
-                    builder: (
-                      BuildContext context,
-                      MenuController controller,
-                      Widget? child,
-                    ) {
-                      return GestureDetector(
-                        onTap: () {
-                          if (controller.isOpen) {
-                            controller.close();
-                          } else {
-                            controller.open();
-                          }
-                        },
-
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundImage: AssetImage("images/default_pfp.jpg"),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          body: SafeArea(
-            child: Stack(
-              children: [
-                Column(
+                    );
+                  },
+                ),
+                title: Row(
                   children: [
-                    // Placeholder for Map
-                    SizedBox(height: 250, width: double.infinity),
-                    ElevatedButton(
-                      onPressed: () {
-                        // updatePaymentOrderData();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PaymentCheckoutScreen()),
-                        );
-                      },
-                      child: const Text("Place Order"),
+                    const Text(
+                      'Roadie',
+                      style: TextStyle(
+                        fontFamily: 'Cursive',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                        fontSize: 24,
+                      ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(16),
-                      color: Colors.white,
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              // Avatar
-                              Container(),
-                              const SizedBox(width: 12),
-                              const Expanded(
-                                child: Text(
-                                  'Your Roadside Assistance Tech: Aaron G.',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Wrap(
-                            spacing: 12,
-                            runSpacing: 12,
-                            children: const [
-                              AssistanceOption(
-                                icon: Icons.local_shipping,
-                                label: 'Towing',
-                              ),
-                              AssistanceOption(
-                                icon: Icons.tire_repair,
-                                label: 'Flat Tire',
-                                selected: true,
-                              ),
-                              AssistanceOption(
-                                icon: Icons.battery_full,
-                                label: 'Battery',
-                              ),
-                              AssistanceOption(
-                                icon: Icons.local_gas_station,
-                                label: 'Fuel',
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.check_circle, color: Colors.green),
-                              SizedBox(width: 8),
-                              Text(
-                                'Assistance is on the way',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Center(
-                            child: Padding(
-                              padding: EdgeInsets.only(top: 4),
-                              child: Text(
-                                'Arriving in 15 min',
-                                style: TextStyle(color: Colors.black54),
-                              ),
+                    const Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'You Are Logged In',
+                          style: TextStyle(fontSize: 12, color: Colors.black54),
+                        ),
+                        Text(
+                          "${user['username']}",
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 8),
+                    MenuAnchor(
+                      controller: _menuController,
+                      style: MenuStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.white),
+                      ),
+                      menuChildren: [
+                        MenuItemButton(
+                          onPressed: () => _onSelected('Your Profile'),
+                          child: Text('Your Profile'),
+                        ),
+                        MenuItemButton(
+                          onPressed: () => _onSelected('Settings'),
+                          child: Text('Settings'),
+                        ),
+                        MenuItemButton(
+                          onPressed: () => _onSelected('Log out'),
+                          child: Text('Log out'),
+                        ),
+                      ],
+                      builder: (
+                        BuildContext context,
+                        MenuController controller,
+                        Widget? child,
+                      ) {
+                        return GestureDetector(
+                          onTap: () {
+                            if (controller.isOpen) {
+                              controller.close();
+                            } else {
+                              controller.open();
+                            }
+                          },
+
+                          child: CircleAvatar(
+                            radius: 18,
+                            backgroundImage: AssetImage(
+                              "images/default_pfp.jpg",
                             ),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
                   ],
                 ),
-              ],
+              ),
+            ),
+            body: SafeArea(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      // Placeholder for Map
+                      SizedBox(height: 250, width: double.infinity),
+                      ElevatedButton(
+                        onPressed: () {
+                          // updatePaymentOrderData();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EnterInfoScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text("Place Order"),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(16),
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                // Avatar
+                                Container(),
+                                const SizedBox(width: 12),
+                                const Expanded(
+                                  child: Text(
+                                    'Your Roadside Assistance Tech: Aaron G.',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            Wrap(
+                              spacing: 12,
+                              runSpacing: 12,
+                              children: const [
+                                AssistanceOption(
+                                  icon: Icons.local_shipping,
+                                  label: 'Towing',
+                                ),
+                                AssistanceOption(
+                                  icon: Icons.tire_repair,
+                                  label: 'Flat Tire',
+                                  selected: true,
+                                ),
+                                AssistanceOption(
+                                  icon: Icons.battery_full,
+                                  label: 'Battery',
+                                ),
+                                AssistanceOption(
+                                  icon: Icons.local_gas_station,
+                                  label: 'Fuel',
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.check_circle, color: Colors.green),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Assistance is on the way',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Center(
+                              child: Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  'Arriving in 15 min',
+                                  style: TextStyle(color: Colors.black54),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
