1c068335ec1cbc8eb4e31df46464469bd3973b3e
Mimo <mimoriam420@gmail.com>
2025-05-03 03:10:39 +0500

    feat(admin): admin update user data

--- lib/screens/admin_panel_screen.dart
+++ lib/screens/admin_panel_screen.dart
@@ -6,7 +6,6 @@ import 'package:pluto_grid/pluto_grid.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:roadee_flutter/draft_local/payment_checkout_screen.draft.dart';
-import 'package:roadee_flutter/screens/home_screen.dart';
 
 class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
@@ -38,7 +37,35 @@ class _AdminScreenState extends State<AdminScreen> {
     return querySnapshot.docs.map((doc) => doc.data()).toList();
   }
 
-  void openGridPopup(BuildContext context, String selectFieldName, var user, int orderIndex) {
+  Future<void> updateAssistantForAnotherUser(String username) async {
+    try {
+      final query =
+      await FirebaseFirestore.instance
+          .collection("users")
+          .where('username', isEqualTo: username)
+          .limit(1)
+          .get();
+
+      final docRef = await query.docs.first.reference;
+      final doc = await docRef.get();
+      final orders = List<Map<String, dynamic>>.from(doc.data()?['orders'] ?? []);
+      final order_idx = doc.data()?["order_index"];
+
+      if (order_idx > 0) {
+        orders[order_idx]["assistant_assigned"] = "${user?["username"]}";
+      }
+
+      await docRef.update({'orders': orders});
+
+      // final orders = List<Map<String, dynamic>>.from(doc['orders']);
+
+      // await docRef.update({
+      //   'address': '161-160, Multan, Punjab, Pakista',
+      // });
+
+    } on FirebaseAuthException {}
+  }
+  void openGridPopup(BuildContext context, String selectFieldName, var queryUser, int orderIndex) {
     // final ordersColumn = PlutoColumn(
     //   title: selectFieldName,
     //   field: selectFieldName,
@@ -48,7 +75,13 @@ class _AdminScreenState extends State<AdminScreen> {
     final List<PlutoColumn> ordersColumn = [];
 
     ordersColumn.addAll([
-      PlutoColumn(title: "Assistant", field: "assistant_assigned", type: PlutoColumnType.select(adminUsers)),
+      PlutoColumn(
+        title: "Assistant",
+        field: "assistant_assigned",
+        type: PlutoColumnType.select(adminUsers),
+        enableEditingMode: true,
+      ),
+      PlutoColumn(title: "Username", field: "username", type: PlutoColumnType.text()),
       PlutoColumn(title: "Service", field: "service", type: PlutoColumnType.text()),
       PlutoColumn(title: "Status", field: "status", type: PlutoColumnType.text()),
     ]);
@@ -67,14 +100,17 @@ class _AdminScreenState extends State<AdminScreen> {
     final ordersRow =
         List.generate(orderIndex, (index) => index)
             .map((index) {
-              if (user["orders"][index + 1]["status"].toString() == OrderStatus.Completed.name) {
+              if (queryUser["orders"][index + 1]["status"].toString() == OrderStatus.Completed.name) {
                 return null;
               }
               return PlutoRow(
                 cells: {
-                  'assistant_assigned': PlutoCell(value: user["orders"][index + 1]["assistant_assigned"]),
-                  'service': PlutoCell(value: user["orders"][index + 1]["service"]),
-                  'status': PlutoCell(value: user["orders"][index + 1]["status"]),
+                  'assistant_assigned': PlutoCell(
+                    value: queryUser["orders"][index + 1]["assistant_assigned"],
+                  ),
+                  'username': PlutoCell(value: queryUser["username"]),
+                  'service': PlutoCell(value: queryUser["orders"][index + 1]["service"]),
+                  'status': PlutoCell(value: queryUser["orders"][index + 1]["status"]),
                 },
               );
             })
@@ -93,6 +129,13 @@ class _AdminScreenState extends State<AdminScreen> {
         event.stateManager.setShowColumnFilter(true);
       },
       onSelected: (PlutoGridOnSelectedEvent event) {},
+
+      onChanged: (PlutoGridOnChangedEvent event) {
+        if (event.column.field == 'assistant_assigned') {
+          final selectedAdmin = event.value;
+          updateAssistantForAnotherUser(event.row.cells['username']?.value);
+        }
+      },
     );
   }
 
@@ -121,11 +164,20 @@ class _AdminScreenState extends State<AdminScreen> {
             children: [
               IconButton(
                 icon: const Icon(Icons.check_box),
-                onPressed: () {
+                onPressed: () async {
                   final row = rendererContext.row;
                   final id = row.cells['order_index']?.value;
 
-                  openGridPopup(context, 'orders', user, id);
+                  final query =
+                      await FirebaseFirestore.instance
+                          .collection("users")
+                          .where('email', isEqualTo: row.cells['email']?.value)
+                          .limit(1)
+                          .get();
+
+                  final queryUser = query.docs.first.data();
+
+                  openGridPopup(context, 'orders', queryUser, id);
                 },
                 iconSize: 18,
                 color: Colors.green,
@@ -146,6 +198,8 @@ class _AdminScreenState extends State<AdminScreen> {
       PlutoColumn(title: 'address', field: 'address', type: PlutoColumnType.text()),
       PlutoColumn(title: 'orders', field: 'orders', type: PlutoColumnType.text()),
     ]);
+
+    setState(() {});
   }
 
   @override
